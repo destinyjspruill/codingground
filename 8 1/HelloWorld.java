3b23bd3d2d99c6a0a02e365ca19d4919ee37b402
destinyjspruill <dspruill29179@jcss.us>
2016-01-06 13:11:28 -0500

    Created by Coding Ground Client

---
+++ 8 1/HelloWorld.java
@@ -0,0 +1,20 @@
+public class HelloWorld{
+
+     public static void main(String []args){
+         int [] classGrades = new int[10]
+         classGrades[0] = 95;
+         classGrades[1] = 42;
+         classGrades[2] = 57;
+         classGrades[3] = 69;
+         classGrades[4] = 100;
+         classGrades[5] = 55;
+         classGrades[6] = 87;
+         classGrades[7] = 93;
+         classGrades[8] = 87;
+         classGrades[9] = 88;
+         
+         System.out.println(classGrades[1]);
+         System.out.println(classGrades[3]);
+         System.out.println(classGrades[5]);
+   
+}
